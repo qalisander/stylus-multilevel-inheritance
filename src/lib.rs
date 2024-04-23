e5ac755687e93636b64f4c630c558fd8e2805589
Alisander Qoshqosh <qalisander@gmail.com>
2024-04-23 20:07:08 +0400

    ++

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-#![cfg_attr(not(test), no_main, no_std)]
+#![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-use stylus_sdk::prelude::{entrypoint, external, sol_storage};
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
+
+use stylus_sdk::prelude::*;
 
 sol_storage! {
     #[entrypoint]
